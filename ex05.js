c19a14c3be1b148dfbfdfc53b23041aa483d4364
BiscuitCuit15 <145538077+LUCA-DesComBes@users.noreply.github.com>
2025-03-19 10:18:39 +0100

    feat(app): ex05 finished

---
+++ ex05.js
@@ -0,0 +1,15 @@
+function listerPersonnes(personnes) {
+    let result;
+	for(let i = 0; i < personnes.length; i++) {
+         result += `${personnes[i].nom} : ${personnes[i].age} ans,`
+    }
+    return result;
+}
+
+const personnes = [
+	{ nom: "Alice", age: 30 },
+	{ nom: "Bob", age: 25 },
+	{ nom: "Charlie", age: 35 }
+];
+
+console.log(listerPersonnes(personnes)); // 'Alice : 30 ans, Bob : 25 ans, Charlie : 35 ans.'
