f9b7e15c767c57f5e46a74f81fb1cbc481a89d3b
BiscuitCuit15 <145538077+LUCA-DesComBes@users.noreply.github.com>
2025-03-19 11:00:41 +0100

    repear (app) : ex08 encore

--- ex08.js
+++ ex08.js
@@ -5,7 +5,7 @@ function afficherProprietes(obj) {
             result += `${key}: ${obj[key]}, `;
         }
     }
-    return result.slice(0, -2); // On retire la dernière virgule et l'espace
+    return result.slice(0, -2);
 }
 
 console.log(afficherProprietes({ nom: "Alice", age: 30, ville: "Paris" }));
