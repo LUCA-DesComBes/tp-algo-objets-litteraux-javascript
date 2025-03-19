b7c8daf7e6ff38b993e6c277589003387cfde860
BiscuitCuit15 <145538077+LUCA-DesComBes@users.noreply.github.com>
2025-03-19 10:13:22 +0100

    repear (app) : ex04

--- ex04.js
+++ ex04.js
@@ -1,9 +1,10 @@
 function trouverPersonne(personnes, nom) {
     for(let i = 0; i < personnes.length; i++) {
-        if (personnes[i].nom == nom) {      
+        if (personnes[i].nom === nom) {      
             return personnes[i];      
-        }   
+        }
     }
+    return null
 }
 
 // Tests
