46dea38eb630f673e81ddaceff725b3e364e7779
BiscuitCuit15 <145538077+LUCA-DesComBes@users.noreply.github.com>
2025-03-19 10:55:15 +0100

    repear (app) : ex08

--- ex08.js
+++ ex08.js
@@ -1,6 +1,5 @@
 function afficherProprietes(obj) {
-	let result = "";
-    return result += `nom: ${obj.nom}, age: ${obj.age}, ville: ${obj.ville}`;
+    return `nom: ${obj.nom}, age: ${obj.age}, ville: ${obj.ville}`;
 }
 
 console.log(afficherProprietes({ nom: "Alice", age: 30, ville: "Paris" })); // 'nom: Alice, age: 30, ville: Paris'
